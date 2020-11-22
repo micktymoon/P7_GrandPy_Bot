2ed22789f08a6056ec85c6a9b7954778114fc860
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-11-22 19:21:25 +0100

    commit pycharm

---
+++ test_accueil.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def test_accroche():
+    url = "http://127.0.0.1:5000/"
+    result = requests.get(url)
+    r_soup = BeautifulSoup(result.text, "html.parser")
+    tag = r_soup.h1
+    print(tag.string)
+    assert tag.string == "GrandPyBot raconte moi une histoire!"
