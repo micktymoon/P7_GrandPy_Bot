4fb2374d52c779137c365ee546057b53af8db7f1
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-12-12 20:08:28 +0100

    modif darius

--- app/maps/maps_api.py
+++ app/maps/maps_api.py
@@ -3,6 +3,9 @@ import googlemaps
 
 def recup_latlong(lieu):
     gmaps = googlemaps.Client(key='AIzaSyAYIr_H7RBFICU0eGWe7hrm6a4AuibiQjI')
+    # pyignore: unresolved
     adresse = gmaps.geocode(lieu)
     location = adresse[0]['geometry']['location']
     return location
+
+print(recup_latlong('OpenClassrooms'))
