37f8c75c9c8a7139e8a97f3a95df323234e82466
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-12-13 20:03:15 +0100

    séparation de la fonction get_info en 2 fct. et ajout de la doc.

--- app/wikipedia/wiki_api.py
+++ app/wikipedia/wiki_api.py
@@ -1,10 +1,22 @@
 import requests
+import re
 
 
-def recup_info_wiki(latitude, longitude):
-    url_api = "https://fr.wikipedia.org/w/api.php"
+def get_pageid(latitude, longitude):
+    """ Return the ID of a Wikipedia page.
+
+    Get the ID of the Wikipedia page of a place given by its latitude and longitude.
+
+    :param latitude: The latitude of the place.
+    :type latitude: int
+    :param longitude: The longitude of the place.
+    :type longitude: int
+    :return: The ID of the Wikipedia page.
+    :rtype: int
+    """
     latitude = latitude
     longitude = longitude
+    url_api = "https://fr.wikipedia.org/w/api.php"
     params_geo = {'format': 'json',
                   'action': 'query',
                   'list': 'geosearch',
@@ -13,6 +25,20 @@ def recup_info_wiki(latitude, longitude):
     response_geo = requests.get(url=url_api, params=params_geo)
     geosearch_data = response_geo.json()
     page_id = geosearch_data['query']['geosearch'][0]['pageid']
+    return page_id
+
+
+def get_history(page_id):
+    """ Return the historical part of the information of a Wikipedia page.
+
+    Get the historical part of the information of a Wikipedia page using its ID.
+
+    :param page_id: The ID of the Wikipedia page.
+    :type page_id: int
+    :return: The historical part of the information of the Wikipedia page.
+    :rtype: str
+    """
+    url_api = "https://fr.wikipedia.org/w/api.php"
     params_page = {"format": "json",
                    "action": "query",
                    "prop": "extracts|info",
@@ -26,7 +52,13 @@ def recup_info_wiki(latitude, longitude):
     for item in extract_data['query']['pages']:
         page_id2 = item
     data = extract_data['query']['pages'][page_id2]
-    return data['extract']
+    sentence = re.split("== Historique ==\n", data['extract'])
+    history = re.split("\n\n\n", sentence[1])
+    return history[0]
 
 
-print(recup_info_wiki(48.8975156, 2.3833993))
+
+
+
+
+
