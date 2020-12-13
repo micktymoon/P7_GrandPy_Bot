ef19797e630c384f43e6b572f84a976dba0e1c24
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-12-13 22:14:51 +0100

    modif de l'utilisation de l'api. suppression de l'utilisation du package googlemaps et utilisation de request à la place

--- app/maps/maps_api.py
+++ app/maps/maps_api.py
@@ -1,10 +1,21 @@
-import googlemaps
+import requests
 
 
-def recup_latlong(lieu):
-    gmaps = googlemaps.Client(key='AIzaSyAYIr_H7RBFICU0eGWe7hrm6a4AuibiQjI')
-    # pyignore: unresolved
-    adresse = gmaps.geocode(lieu)
-    location = adresse[0]['geometry']['location']
-    return location
+def get_location(place):
+    """ Return the location and address of a place.
+
+    Get the
+
+    :param place:
+    :return:
+    """
+    api_key = 'AIzaSyAYIr_H7RBFICU0eGWe7hrm6a4AuibiQjI'
+    api_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    params = {'address': place,
+              'key': api_key}
+    response = requests.get(api_url, params)
+    response_json = response.json()
+    address = response_json['results'][0]['formatted_address']
+    location = response_json['results'][0]['geometry']['location']
+    return {'location': location, 'address': address}
 
